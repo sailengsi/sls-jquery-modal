28dc6185ef8e05edbe3ad084d90497e7cd75007b
赛冷思 <liumh1011@163.com>
2016-10-14 18:13:14 +0800

    jquery弹框插件。

---
+++ dev/fis-conf.js
@@ -0,0 +1,89 @@
+//所有需要操作的信息都装到这个对象里边，方便管理
+var fisSource = {
+
+    //压缩js和css
+    uglify: {
+        //压缩开关,为true时才执行压缩
+        flag: true,
+        //需要压缩的js目录
+        js: [
+            'js/*.js',
+        ],
+        //需要压缩的css目录
+        css: []
+    },
+
+    //打包
+    pack: {
+        //打包开关
+        flag: true,
+
+        /**
+         * [content pack resource]
+         * @type    {Object}
+         * @key     {string}            [打包后存放地方]
+         * @value   {array or string}   [需要打包的资源]
+         */
+        content: {
+            'dist/js/jquery.sls-modal.min.js': [
+                'src/js/jquery.sls-modal.js',
+            ]
+        }
+    }
+}
+
+//压缩
+var uglifyFlag = fisSource.uglify.flag;
+if (uglifyFlag) {
+    var uglifyJs = fisSource.uglify.js;
+    var uglifyCss = fisSource.uglify.css;
+
+    for (var i = 0; i < uglifyJs.length; i++) {
+        fis.match(uglifyJs[i], {
+            optimizer: fis.plugin('uglify-js'),
+        });
+    }
+
+    for (var i = 0; i < uglifyCss.length; i++) {
+        fis.match(uglifyCss[i], {
+            optimizer: fis.plugin('uglify-css'),
+        });
+    }
+}
+
+//打包
+var depsPack = {
+    // useSourceMap: true
+};
+var packFlag = fisSource.pack.flag;
+var pack = {};
+if (packFlag) {
+    var packs = fisSource.pack.content;
+    for (var path in packs) {
+        depsPack[path] = packs[path];
+    }
+}
+
+
+//js,css合并后，在发布之后的html文件里，自动引入合并之后的文件
+fis.match('::packager', {
+    postpackager: fis.plugin('loader'),
+    packager: fis.plugin('deps-pack', depsPack)
+});
+
+
+//设置html引入资源路径保持相对路径
+fis.hook("relative");
+
+fis.match("**", {
+    relative: true,
+    deploy: [
+        fis.plugin('skip-packed', {
+            // 配置项
+        }),
+
+        fis.plugin('local-deliver', {
+            to: '../release'
+        })
+    ]
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+文档地址：doc.sailengsi.com/sls-jquery-modal-doc/
+
+demo演示地址：demo.sailengsi.com/sls-jquery-modal/release
